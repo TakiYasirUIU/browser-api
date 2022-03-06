1a35075ed8301a7911cf7625ee7d98865c6be983
Taki Yasir <95402895+TakiYasirUIU@users.noreply.github.com>
2022-03-06 21:00:55 +0600

    initial commit

---
+++ explore.js
@@ -0,0 +1,8 @@
+const name =15;
+const student ={name: 'jolil', friend: 'borha', job: 'Business',
+    makeMOvie: function(name){
+    console.log(name);
+    }
+};
+student.makeMOvie('Din raat');
+document.getElementById('name');
